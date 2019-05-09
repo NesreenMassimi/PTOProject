c7cc87175583d32a88c0715ccde1566d9e5f8d54
NesreenMassimi <nmosimi@gmail.com>
2019-05-09 14:52:22 +0300

    initial settings

--- PTOProject/settings.py
+++ PTOProject/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'PTOProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PTO_system',
+        'USER': 'root',
+        'PASSWORD': '16001700',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
     }
 }
 
--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey,DATE,DATETIME,Float
+from sqlalchemy.ext.declarative import declarative_base
+import pymysql
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import create_engine
+engine = create_engine("mysql://root:16001700@localhost/test")
+Base = declarative_base()
+
 
 # Create your models here.
